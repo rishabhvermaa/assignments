e07ad2b9887970b430637f9ccf8f575fa50c17cb
RishabhVermaa <rishabh.verma.rsv@gmail.com>
2024-01-13 21:32:29 +0530

    Added solution for largest number

--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -6,7 +6,16 @@
 */
 
 function findLargestElement(numbers) {
-    
+    let max = numbers[0];
+    let i;
+    for(i = 0; i < numbers.length; i++){
+        if(numbers[i] > max){
+            max = numbers[i];
+        }
+    }
+    return max;    
 }
+
+// console.log(findLargestElement([3.5, 7.2, 2.1, 9.8, 1.9]));
 
 module.exports = findLargestElement;
